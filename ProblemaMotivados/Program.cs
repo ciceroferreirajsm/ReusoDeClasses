f306878bb39a1416093f0714b748e1c65890a7cf
Cicero ferreira <ciceronascimentu@gmail.com>
2020-12-17 13:28:55 -0300

    Adicionar arquivos de projeto.

---
+++ ProblemaMotivados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProblemaMotivados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> printService = new PrintService<string>();
+
+            Console.Write("How many value: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite Os nomes: ");
+                string x = Console.ReadLine();
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
